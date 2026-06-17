ca32aeb467533608f8667e80d2115334aae3ecb1
beyzaaslantatar <124090733+beyzaaslantatar@users.noreply.github.com>
2026-06-17 14:35:03 +0300

    entities created

---
+++ Core/YoutubeApiDomain/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApiDomain.Common;
+using YoutubeApiDomain.Entities;
+
+namespace YoutubeApiDomain.Entities
+{
+    public class Category : EntityBase, IEntityBase
+    {
+
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            Name = name;
+            Priority = priority;
+            ParentId = parentId;
+            
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products{ get; set; }
+    }
+}

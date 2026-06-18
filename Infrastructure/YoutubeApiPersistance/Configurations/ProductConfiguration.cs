a6727e65938dc5cee6afe46e2dfbe792e1a5fa6e
beyzaaslantatar <124090733+beyzaaslantatar@users.noreply.github.com>
2026-06-18 11:46:17 +0300

    Dbcontext has created

---
+++ Infrastructure/YoutubeApiPersistance/Configurations/ProductConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApiDomain.Entities;
+
+namespace YoutubeApiPersistance.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new Faker("tr");
+
+
+            Product product1 = new()
+            {
+                Id = 1,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 1,
+                Price = faker.Finance.Amount(10,1000),
+                Discount = faker.Random.Decimal(0,10),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+            Product product2 = new()
+            {
+                Id = 2,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 2,
+                Price = faker.Finance.Amount(10, 1000),
+                Discount = faker.Random.Decimal(0, 10),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+            Product product3 = new()
+            {
+                Id = 3,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 3,
+                Price = faker.Finance.Amount(10, 1000),
+                Discount = faker.Random.Decimal(0, 10),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+
+            builder.HasData(product1,product2,product3);
+
+        }
+    }
+}
---
+++ Infrastructure/YoutubeApiPersistance/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApiPersistance.Context;
+
+namespace YoutubeApiPersistance
+{
+    public static class Registration
+    {
+        public static void AddPersistance(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt => 
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}

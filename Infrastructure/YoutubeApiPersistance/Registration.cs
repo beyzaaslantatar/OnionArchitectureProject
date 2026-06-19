9d4303c0af9150bcb2026cc716a6a3632b2c9793
beyzaaslantatar <124090733+beyzaaslantatar@users.noreply.github.com>
2026-06-19 11:40:40 +0300

    write repository done

--- Infrastructure/YoutubeApiPersistance/Registration.cs
+++ Infrastructure/YoutubeApiPersistance/Registration.cs
@@ -20,6 +20,8 @@ namespace YoutubeApiPersistance
             opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped(typeof(IReadRepository<>) , typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
         }
     }
 }
